642476a8a6a85580ec9995f2c417e171ab12ab4f
k.d.v.kumar123@gmail.com <k.d.v.kumar123@gmail.com>
2020-12-17 22:33:46 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 8080
-
+const { newsArticleModel } = require('./connector')
 const onePageArticleCount = 10
 
 
@@ -9,7 +9,45 @@ const onePageArticleCount = 10
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
+app.get("/newFeeds", (req, res) => {
+    let limitReceived = req.query.limit;
+    let offsetReceived = req.query.offset;
+    let limit = parseInt(Number(limitReceived));
+    let offset = parseInt(Number(offsetReceived));
+    if (!limitReceived) {
+      limit = onePageArticleCount;
+    }
+    if (!offsetReceived) {
+      offset = 0;
+    }
+    if (isNaN(offset)) {
+      offset = 0;
+    }
+    if (isNaN(limit) || limit < 0) {
+      limit = onePageArticleCount;
+      if (!offsetReceived) {
+        offset = 0;
+      }
+    }
+  
+    const options = {};
+    options.skip = offset;
+    options.limit = limit;
+    newsArticleModel.countDocuments({}, function (err) {
+      if (err) {
+        res.status(500).send({ message: err.message });
+        return;
+      }
+      newsArticleModel.find({}, {}, options, function (err, result) {
+        if (err) {
+          res.status(500).send({ message: err.message });
+          return;
+        } else {
+          res.status(200).send(result);
+        }
+      });
+    });
+  });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
